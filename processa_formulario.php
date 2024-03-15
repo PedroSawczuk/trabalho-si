d05dd22acb22bf7b5da30ae119b5c41ef398e148
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2024-03-15 17:58:30 -0400

    Secreto - Processa formulário

--- processa_formulario.php
+++ processa_formulario.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
+
 <body>
-    <button><a href="index.php">Voltar</a></button>
+    <button><a href="index.php" class="btn btn-primary">Voltar</a></button>
+    
+    <!-- Adicione o link para os scripts do Bootstrap (JQuery e Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- secreto.php
+++ secreto.php
@@ -20,20 +20,20 @@ if (isset($_GET['nome']) && isset($_GET['email']) && isset($_GET['idade'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário GET com Bootstrap</title>
-    <!-- Adicionando CSS do Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0Z1d9vrScQmbxC3VrJfEqKRBK2KnUe4rNE6K+CwYxAl/Y+2QPk60sYyh0Sc5b" crossorigin="anonymous">
+    <!-- Adicionando Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container mt-5">
         <form action="processa_formulario.php" method="GET">
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome:</label>
+            <div class="form-group">
+                <label for="nome">Nome:</label>
                 <input type="text" id="nome" name="nome" class="form-control">
             </div>
 
-            <div class="mb-3">
-                <label for="email" class="form-label">Email:</label>
+            <div class="form-group">
+                <label for="email">Email:</label>
                 <input type="email" id="email" name="email" class="form-control">
             </div>
 
@@ -41,8 +41,10 @@ if (isset($_GET['nome']) && isset($_GET['email']) && isset($_GET['idade'])) {
         </form>
     </div>
 
-    <!-- Adicionando JavaScript do Bootstrap (opcional) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+EsOIjizsH/8F8N7pKxF4OeLrjIZnE4heB+tFfs" crossorigin="anonymous"></script>
+    <!-- Adicionando Bootstrap JS (opcional, caso necessite de funcionalidades JavaScript do Bootstrap) -->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script> -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 </body>
 
 </html>
